e9641fd29e0f44e6950a7fb536c90a4fba756116
LILY HUANG <1466094598lily.e@gmail.com>
2025-10-29 09:42:06 +0000

    File changes

--- src/pages/QuestBoard.jsx
+++ src/pages/QuestBoard.jsx
@@ -58,7 +58,7 @@ export default function QuestBoard() {
   });
 
   const handleQuestsGenerated = (generatedQuests) => {
-    setPendingQuests(generatedQuests);
+    setPendingQuests(prev => [...prev, ...generatedQuests]);
   };
 
   const confirmQuests = () => {

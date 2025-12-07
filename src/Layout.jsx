df0cc941d5fd333045469b9797c22c0edc9ed2a4
LILY HUANG <1466094598lily.e@gmail.com>
2025-12-07 10:12:03 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,7 +7,6 @@ import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { initAudioManager } from "@/components/AudioManager";
-import { Helmet } from "react-helmet";
 
 function LayoutContent({ children }) {
   const location = useLocation();

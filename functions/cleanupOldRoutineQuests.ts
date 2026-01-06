37732bd2dab408b869b37f4521d8af757c20518c
LILY HUANG <1466094598lily.e@gmail.com>
2026-01-06 14:55:38 +0000

    File changes

--- functions/cleanupOldRoutineQuests.ts
+++ functions/cleanupOldRoutineQuests.ts
@@ -10,14 +10,22 @@ Deno.serve(async (req) => {
       return Response.json({ error: 'Forbidden: Admin access required' }, { status: 403 });
     }
 
-    // TODO: Query all isRoutine: true quests
+    // Query all routine quests using service role
+    console.log('Starting cleanup: Querying all routine quests...');
+    const routineQuests = await base44.asServiceRole.entities.Quest.filter({ 
+      isRoutine: true 
+    }, '-created_date', 10000); // Large limit to get all routine quests
+    
+    console.log(`Found ${routineQuests.length} routine quests`);
+
     // TODO: Group by originalActionHint
     // TODO: Identify latest template in each group
     // TODO: Delete old versions
 
     return Response.json({ 
       success: true,
-      message: 'Cleanup function initialized'
+      message: `Found ${routineQuests.length} routine quests`,
+      foundCount: routineQuests.length
     });
   } catch (error) {
     console.error('Cleanup error:', error);
